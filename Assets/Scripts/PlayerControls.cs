ba3fac424829f0f76deb6628d66fe63ff125d6ae
Einars Koops <mankomancer@gmail.com>
2023-12-07 17:43:02 +0200

    Added player keyboard control, made spawning tiles work. Next one should be movement of tiles and their merges

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] bool allowPlayerControl=true;
+    public bool AllowPlayerControl{ //playing with this way how to isolate data
+        get {return allowPlayerControl;}
+        set {allowPlayerControl=value;}
     }
 
+
+    float horizonatInput;
+    float verticalInput;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (AllowPlayerControl){
+            if (Input.GetKeyUp(KeyCode.W)){ //GetKeyUp is used, so doesnt accept input command all the time
+                AllowPlayerControl=false;
+                GameObject.FindAnyObjectByType<TilesManager>().MoveUp();
+            }
+            else if(Input.GetKeyUp(KeyCode.D)){
+                AllowPlayerControl=false;
+                GameObject.FindAnyObjectByType<TilesManager>().MoveRight();
+            }
+            else if(Input.GetKeyUp(KeyCode.A)){
+                AllowPlayerControl=false;
+                GameObject.FindAnyObjectByType<TilesManager>().MoveLeft();
+            }
+            else if(Input.GetKeyUp(KeyCode.S)){
+                AllowPlayerControl=false;
+                GameObject.FindAnyObjectByType<TilesManager>().MoveDown();
+            }
+        }
     }
+
+    
 }
--- Assets/Scripts/TilesManager.cs
+++ Assets/Scripts/TilesManager.cs
@@ -1,30 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using Unity.VisualScripting;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class TilesManager : MonoBehaviour
 {
     [SerializeField] private GameObject parentObject; //so all tiles spawn under parent object
-    [SerializeField] GameObject tile2;
+    [SerializeField] GameObject tile2;  //used to spawn default tyles
+    [SerializeField] GameObject playerControls;
+
 
     //Could use this to get coordinates for spawn points
     [SerializeField] GameObject[] cells;
+    bool[] fieldRepresenter = new bool [16]; //field count.  true - fillable, false - not fillable
 
     int randomNumber;
-    GameObject spawnTile;
+
+    GameObject tileSpawn;
 
 
     void Start()
     {
-        randomNumber = Random.Range(0,16);
-        UnityEngine.Vector2 randomPosition = new UnityEngine.Vector2 (cells[randomNumber].transform.position.x, cells[randomNumber].transform.position.y);
-        spawnTile = Instantiate(tile2, randomPosition, UnityEngine.Quaternion.identity,parentObject.transform);
-        
-        /*GameObject spawn;
-        Vector2 randomPosition = new Vector2(Random.Range(-5,5), Random.Range(-5,5));
-        spawn = Instantiate(tile2, randomPosition ,Quaternion.identity,parentObject.transform);
-        */
+        for (int i = 0; i<=15; i++){    //fill with default "empty" value
+            fieldRepresenter[i]=true;
+        }
+        SpawnTile();
 
     }
 
@@ -35,23 +37,68 @@ public class TilesManager : MonoBehaviour
     }
 
     public void SpawnTile(){
+        
+        //check possible spawn number
+        randomNumber = Random.Range(0,16);
+        int i = 15;
+        do{
+            if (fieldRepresenter[randomNumber]){ //we found a field where a tile can spawn can stop operation
+                break;
+            }
+            else{   //if we didnt find where tyle can spawn with first time then we pick next avaiable value
+                if(randomNumber==15){
+                    randomNumber=0;
+                }
+                else if(randomNumber<15){
+                    randomNumber++;
+                }
+                i--;
+            }
+        }while(i!=0);
 
+        if (!fieldRepresenter[randomNumber]){ //we double check if spawn point exists, if it doesnt then we leave this function
+            //ADD YOU LOSE SCREEN HERE
+            Debug.Log("You LOST");
+            return;
+        }
+
+        UnityEngine.Vector2 randomPosition = new UnityEngine.Vector2 (cells[randomNumber].transform.position.x, cells[randomNumber].transform.position.y);
+        tileSpawn = Instantiate(tile2, randomPosition, UnityEngine.Quaternion.identity,parentObject.transform);
+        fieldRepresenter[randomNumber]=false;
+
+        //if no number possible - game over
+
+        //if number possible spawn tile
+        
+        /*  this works just commented out to test stuff
+        randomNumber = Random.Range(0,16);
+        UnityEngine.Vector2 randomPosition = new UnityEngine.Vector2 (cells[randomNumber].transform.position.x, cells[randomNumber].transform.position.y);
+        tileSpawn = Instantiate(tile2, randomPosition, UnityEngine.Quaternion.identity,parentObject.transform);
+        */
     }
 
     public void MoveRight(){
 
+        SpawnTile(); //need to have new tile before player is allowed to move
+        playerControls.GetComponent<PlayerControls>().AllowPlayerControl=true; //enables player movement only after all previous actions have been taken
     }
 
     public void MoveLeft(){
 
+        SpawnTile();
+        playerControls.GetComponent<PlayerControls>().AllowPlayerControl=true;
     }
 
     public void MoveUp(){
 
+        SpawnTile();
+        playerControls.GetComponent<PlayerControls>().AllowPlayerControl=true;
     }
 
     public void MoveDown(){
 
+        SpawnTile();
+        playerControls.GetComponent<PlayerControls>().AllowPlayerControl=true;
     }
 
 
